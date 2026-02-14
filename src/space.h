8e34586ee3334f6f0ea237582972418c95256968
JustinLai <justinlth3@gmail.com>
2026-02-14 16:08:25 +0000

    Added space with sqlite.

---
+++ src/space.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "QDir"
+#include "SQLiteCpp/SQLiteCpp.h"
+#include "QSharedPointer"
+
+namespace NTA
+{
+    constexpr unsigned long applicationId = 0x4E5441;
+
+    class Space
+    {
+    public:
+        static Space* createStorage(QDir path, QString name);
+        ~Space();
+        QSharedPointer<SQLite::Database> getFile() { return file; }
+
+    protected:
+        explicit Space(const QSharedPointer<SQLite::Database>& in_file);
+
+    protected:
+        QSharedPointer<SQLite::Database> file;
+    };
+} // NTA
