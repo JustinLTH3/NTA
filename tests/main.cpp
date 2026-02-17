ba7218ae7c3d3c131b944dbfe14ec1474a675ba3
JustinLai <justinlth3@gmail.com>
2026-02-17 14:48:01 +0000

    Add test cases.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include "createspacetest.h"
 #include "openspacetest.h"
+#include "testcreatenote.h"
 
 int main(int argc, char* argv[])
 {
@@ -7,5 +8,7 @@ int main(int argc, char* argv[])
     QTest::qExec(&test1, argc, argv);
     OpenSpaceTest test2;
     QTest::qExec(&test2, argc, argv);
+    TestCreateNote test3;
+    QTest::qExec(&test3, argc, argv);
     return 0;
 }
---
+++ tests/testcreatenote.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <QTest>
+
+namespace NTA
+{
+    class Space;
+}
+
+class TestCreateNote : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void init();
+    void createDefaultNoteTest();
+    void createNoteWithTitleTest();
+    void createNoteWithOtherTypeIdTest();
+    void createNoteWithInvalidTypeIdTest();
+    void cleanup();
+
+private:
+    QSharedPointer<NTA::Space> space;
+};
