77ce93c2f24d51767e8946f25ad7394cc497bd9f
JustinLai <justinlth3@gmail.com>
2026-03-02 13:47:43 +0000

    Add comment in space

--- src/space.h
+++ src/space.h
@@ -23,14 +23,39 @@ namespace NTA
          */
         static Space* openExistingSpace(const QString& path);
         ~Space();
+        /**
+         * @return The database connection.
+         */
         QSharedPointer<SQLite::Database> getFile() { return file; }
 
+        /**
+         * @param typeId The type of the note. Must be existed in the note_type table.
+         * @param title The title of the note. There can be notes with the same title.
+         * @return The created note. Nullptr if not successful
+         */
         QSharedPointer<Note> createNote(int64_t typeId = 1, const QString& title = "New note");
+        /**
+         * @param id The id of the note.
+         * @return The retrieved note with all content. Nullptr if note not exists.
+         */
         QSharedPointer<Note> getNoteWithId(int64_t id);
+        /**
+         * @param param Text for searching notes that match param. Title and body are considered.
+         * @return A sqlite statement for execution. Only contains id and is ordered by rank.
+         */
         SQLite::Statement searchNotes(const QString& param);
+        /**
+         * @param from The source id of the link.
+         * @param to The target id of the link.
+         * @return Success or not.
+         */
         bool addLink(int64_t from, int64_t to);
 
     protected:
+        /**
+         * It is protected for preventing space initialization other than the static createSpace/openExistingSpace function.
+         * @param in_file the database connection.
+         */
         explicit Space(const QSharedPointer<SQLite::Database>& in_file);
 
     protected:
