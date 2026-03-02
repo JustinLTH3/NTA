c930a47dbfe12d7efd3e4133c109a524afca6239
JustinLai <justinlth3@gmail.com>
2026-03-02 14:12:03 +0000

    Add start window class

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 ﻿#include <QApplication>
-#include <QPushButton>
+
+#include "nstartwindow.h"
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    NTA::NStartWindow startWindow;
+    startWindow.show();
     return QApplication::exec();
 }
---
+++ src/nstartwindow.cpp
@@ -0,0 +1,21 @@
+﻿//
+// Created by JustinLai on 2/3/2026.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_NStartWindow.h" resolved
+
+#include "nstartwindow.h"
+#include "ui_NStartWindow.h"
+
+namespace NTA
+{
+    NStartWindow::NStartWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::NStartWindow)
+    {
+        ui->setupUi(this);
+    }
+
+    NStartWindow::~NStartWindow()
+    {
+        delete ui;
+    }
+} // NTA
