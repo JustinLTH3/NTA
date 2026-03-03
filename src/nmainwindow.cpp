b2e6a76384c2096fb7abe325dd46fadf417647f0
JustinLai <justinlth3@gmail.com>
2026-03-03 15:17:38 +0000

    Add main window class

---
+++ src/nmainwindow.cpp
@@ -0,0 +1,13 @@
+﻿#include "nmainwindow.h"
+#include "ui_NMainWindow.h"
+
+
+NMainWindow::NMainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::NMainWindow)
+{
+    ui->setupUi(this);
+}
+
+NMainWindow::~NMainWindow()
+{
+    delete ui;
+}
