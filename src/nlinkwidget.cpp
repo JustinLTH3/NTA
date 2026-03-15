e87c2f2717b16ab741fa029fccbc49457d7b7099
JustinLai <justinlth3@gmail.com>
2026-03-15 15:13:40 +0000

    Add link widget

---
+++ src/nlinkwidget.cpp
@@ -0,0 +1,46 @@
+﻿//
+// Created by JustinLai on 15/3/2026.
+//
+
+#include "nlinkwidget.h"
+
+#include <QVBoxLayout>
+
+#include "ElidingLabel.h"
+
+namespace NTA
+{
+    NLinkWidget::NLinkWidget(ads::CDockManager* dockManager, const QString& title, QWidget* parent) : NWidget(
+        dockManager, title, parent)
+    {
+        auto central = new QWidget(this);
+        setWidget(central);
+        central->setLayout(new QVBoxLayout(central));
+        listWidget = new QListWidget(central);
+        central->layout()->addWidget(listWidget);
+    }
+
+    NLinkWidget::~NLinkWidget()
+    {
+    }
+
+    void NLinkWidget::linkNote(const QSharedPointer<Note>& inNote, bool linked)
+    {
+        NWidget::linkNote(inNote, linked);
+    }
+
+    void NLinkWidget::setNote(const QSharedPointer<Note>& inNote)
+    {
+        NWidget::setNote(inNote);
+    }
+
+    void NLinkWidget::onFocusNoteChanged(int64_t old, int64_t now)
+    {
+
+    }
+
+    void NLinkWidget::onNoteUpdated(int64_t id, NWidget* from, unsigned int columns)
+    {
+        NWidget::onNoteUpdated(id, from, columns);
+    }
+} // NTA
---
+++ src/nlinkwidget.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <QListWidget>
+
+#include "nwidget.h"
+
+namespace NTA
+{
+    class NLinkWidget : public NWidget
+    {
+        Q_OBJECT
+
+    public:
+        explicit NLinkWidget(ads::CDockManager* dockManager, const QString& title, QWidget* parent = nullptr);
+        ~NLinkWidget() override;
+        void linkNote(const QSharedPointer<Note>& inNote, bool linked) override;
+        void setNote(const QSharedPointer<Note>& inNote) override;
+
+    protected slots:
+        void onFocusNoteChanged(int64_t old, int64_t now) override;
+        void onNoteUpdated(int64_t id, NWidget* from, unsigned int columns) override;
+
+    protected:
+        QListWidget* listWidget = nullptr;
+    };
+} // NTA
